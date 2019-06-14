a441c7f69535b20b6bcd7ab7e3952428aa610b8e
Hjyn55w <948950540@qq.com>
2019-06-14 11:07:20 +0800

    这一周的扑克牌和棋盘

---
+++ 第6周 JS/Hikaru no Go/pan.js
@@ -0,0 +1,123 @@
+
+$(function () {
+    let box = $('.box');
+    let flag = true;
+    let black = {} ,white = {};
+    let blank = {};
+    let ai = true;
+    for (let i=0;i<25;i++){
+        for (let j=0;j<25;j++){
+            $('<div>').addClass('chess').attr('id',i+'_'+j).appendTo(box);
+            blank[i+'_'+j] = true;
+        }
+    }
+    // 下棋
+    box.on('click' , '.chess' , function () {
+        let _this = $(this);
+        if (_this.hasClass('black') || _this.is('.white')) {
+            return;
+        }
+        flag = !flag;
+        let coords = _this.attr('id');
+
+        if (flag){
+            black[coords] = true;
+            delete blank[coords];
+            $(this).addClass('black');
+            if(Victory(black , coords) >=5 ){
+                console.log('黑棋胜');;
+                box.off('click');
+            }
+        } else{
+            white[coords] = true;
+            delete blank[coords];
+            $(this).addClass('white');
+            console.log(white);
+            if(Victory(white , coords) >=5 ){
+                console.log('白棋胜');;
+                box.off('click');
+            }
+            if (ai){
+                let pos = AI();
+                black[pos] = true;
+                delete blank[pos];
+                $('#' + pos).addClass('black');
+                console.log(pos);
+                if (Victory(black , pos) >=5){
+
+                    box.off('click');
+                    console.log('黑棋胜');;
+                }
+                flag = !flag;
+            }
+        }
+    });
+
+    // AI判断
+    function AI() {
+        let blankScore = 0 , whiteScore = 0;
+        let pos1 = '' , pos2 = '';
+        for (let i in blank){
+            let score = Victory(black,i);
+            if (score >= blankScore){
+                blankScore = score;
+                pos1 = i;
+            }
+        }
+        for (let i in blank){
+            let score = Victory(white,i);
+            if (score >= whiteScore){
+                whiteScore = score;
+                pos2 = i;
+            }
+        }
+        return blankScore >= whiteScore ? pos1 : pos2;
+    }
+
+    function Victory(obj,coords) {
+        let sp=1 ,cz=1 ,yx=1 ,zx =1;
+        let [x , y] = coords.split('_');
+        let i = x*1,j = y*1;
+
+        // sp
+        while(obj[i + '_' + (++j)]){
+            sp++;
+        }
+        j =y*1;
+        while(obj[i+'_'+(--j)]){
+            sp++;
+        }
+        j =y*1;
+
+        // cz
+        while(obj[++i + '_' + j]){
+            cz++;
+        }
+        i = x*1;
+        while(obj[--i + '_' + j]){
+            cz++;
+        }
+
+        i = x*1;
+        j = y*1;
+
+        // yx
+        while(obj[--i + '_' + (++j)]){
+            yx++;
+        }
+        i = x * 1;j = y * 1;
+        while(obj[++i + '_' + (--j)]){
+            yx++;
+        }
+        i = x*1;j = y*1;
+        // zx
+        while (obj[--i+'_'+(--j)]){
+            zx++;
+        }
+        i =x*1;j=y*1;
+        while (obj[++i+'_'+(++j)]){
+            zx++;
+        }
+        return Math.max(sp,cz,zx,yx);
+    }
+});
---
+++ 第6周 JS/扑克牌游戏/Poker.js
@@ -0,0 +1,113 @@
+$(function () {
+
+    let Poker = [];
+    let colorArr = ['s', 'h', 'd', 'c'];
+    let flag = {};
+    let Pad = $('.Pad');
+
+
+    for (let i = 0; i < 52; i++) {
+        let index = Math.floor(Math.random() * colorArr.length);
+        let color = colorArr[index];
+        let number = Math.round(Math.random() * 12 + 1);
+
+        while(flag[color+'_'+number]){
+            index = Math.floor(Math.random() * colorArr.length);
+            color = colorArr[index];
+            number = Math.round(Math.random() * 12 + 1);
+        }
+
+    Poker.push({color, number});
+    flag[color+'_'+number] = true;
+    }
+    console.log(Poker);
+
+    let index = -1;
+    for (let i=0;i<8;i++){
+        for (let j=0;j<=i;j++){
+            index++;
+            let obj = Poker[index];
+            let lefts = 450-50 * i + 100 * j , tops = 50 * i;
+            $('<div>')
+                .addClass('Poke')
+                .css({backgroundImage:`url(../imgs/${obj.number}${obj.color}.jpg)`})
+                .appendTo('.Pad')
+                .data('number' , obj.number)
+                .attr('id',i+'_'+j)
+                .delay(index*100)
+                .animate({left:lefts,top:tops,opacity:1})
+        }
+
+    }
+
+    for (;index<52;index++){
+        let obj = Poker[index];
+        let lefts = 20*(index - 30);
+        $('<div>')
+            .addClass('Poke')
+            .addClass('left')
+            .css({backgroundImage:`url(../imgs/${obj.number}${obj.color}.jpg)`})
+            .appendTo('.Pad')
+            .attr('id','-2_-2')
+            .data('number' , obj.number)
+            .delay(index*120)
+            .animate({left:lefts,top:550,opacity:1})
+    }
+    // 选出牌
+    let first = null;
+    Pad.on('click','.Poke',function () {
+
+        let _this = $(this);
+        let [i,j] = _this.attr('id').split('_');
+        let id1 = i*1 + 1+ '_' +j , id2 = i*1 + 1 + '_' +(j*1+1);
+        console.log(1);
+        if ($('#' + id1).length || $('#' + id2).length) {
+            return;
+        }
+        // $(this).animate({top:'-=30px'});
+        if (_this.hasClass('active')){
+            $(this).removeClass('active').animate({top:'+=30px'})
+        }else{
+            $(this).addClass('active').animate({top:'-=30px'})
+        }
+        // 两张牌是否和为14
+        if (!first){
+            first = _this;
+        }else{
+            console.log(first);
+            let number1 = first.data('number') , number2 = _this.data('number');
+
+
+            if (number1 + number2 === 14){
+                $('.active').animate({top:0,left:800,opacity:0},function () {
+                    $(this).remove();
+                });
+            } else{
+                $('.active').animate({top:'+=30px'},function () {
+                    $(this).removeClass('.active');
+                })
+            }
+            first = null;
+        }
+    });
+    // 切牌
+    let n = 0;
+    let rightBtn = $('.right');
+    let flag1 = true;
+    console.log(rightBtn);
+    rightBtn.on('click',function () {
+        console.log(1);
+
+        console.log(flag1);
+        if (!flag1){
+            return
+        }
+        flag1 = false;
+        $('.left').last().css('zIndex',n++).animate({left:710+(n*10)},function () {
+                $(this).removeClass('left').addClass('right')
+                flag1 = true;
+        })
+
+
+    })
+});
